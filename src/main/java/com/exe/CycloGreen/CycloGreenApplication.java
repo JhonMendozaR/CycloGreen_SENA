1babff90ba4dc3882823cc99d4f54de5be5182cd
Jhon Mendoza <jhon99mendoza@gmail.com>
2025-09-17 22:48:23 -0500

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/exe/CycloGreen/CycloGreenApplication.java
@@ -0,0 +1,13 @@
+package com.exe.CycloGreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycloGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycloGreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe/CycloGreen/CycloGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.CycloGreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycloGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
